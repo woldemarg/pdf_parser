875dde98f7c78374ff197b684daad79567308472
Volodymyr Holomb <wldmrgml@gmail.com>
2020-08-23 16:11:28 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# pdf_parser
-pdf_parser for group_bwt
+## Сравнение модулей
+### tabula-py 1.4.1
+* [oписание и документация](https://tabula-py.readthedocs.io/en/latest/)
+* проблемы:
+    * первая строка распознается как заголовок
+    * объединение нескольких колонок в одну
+    * не распознается продолжение таблицы на след. стр.
+    * в таблицу вставляются колонки с пустыми значениями
+### camelot-py 0.8.2
+* [описание и документация](https://camelot-py.readthedocs.io/en/master/)
+* проблемы:
+    * распознает как строки таблицы весть текст документа
+    * не выделяет колонки
+    * новую стр. в документе относит кновойтаблиуе (не распознает продолжение)
+### PDFLayoutTextStripper (рекомендуется в [описании](task_description/PDF Invoice Parsing.docx) задания)
+* [описание и документация](https://github.com/JonathanLink/PDFLayoutTextStripper) (java class for Apache PDFBox)
+* [python API](https://github.com/thoqbk/PDFLayoutTextStripper)
+* [py4j](https://www.py4j.org/py4j_java_gateway.html#examples) (java to python)
+* проблемы:
+    * ошибка на файле [PO#_077-2706402.pdf(sysco PO#_077-2706402.pdf)](task_description/examples/PO#_077-2706402.pdf):
+    ```python
+    {'success': 'false', 'error': 'String index out of range: -1'}
+    ```
+
+## TODO
+1. Предусмотреть обработку ошибки ```{'success': 'false', 'error': 'String index out of range: -1'}```
+2.
---
+++ scripts/compare_modules.py
@@ -0,0 +1,57 @@
+import tabula
+import camelot
+from py4j.java_gateway import JavaGateway
+
+# %%
+PDF_1 = "task_description/examples/GFS 5760519.pdf"
+PDF_2 = "task_description/examples/sysco PO#_077-2706434.pdf"
+
+# %%
+# tabula-py 1.4.1
+df_1 = tabula.read_pdf(PDF_1, stream=True, pages="all")
+df_1.head()
+
+# %%
+df_2 = tabula.read_pdf(PDF_2, stream=True, pages="all")
+df_2.head()
+
+# %%
+# camelot-py 0.8.2
+tables_1 = camelot.read_pdf(PDF_1,
+                            flavor="stream",
+                            split_text=True,
+                            suppress_stdout=True,
+                            pages="all")
+tables_1[0].df.head(25)
+
+# %%
+tables_2 = camelot.read_pdf(PDF_2,
+                            flavor="stream",
+                            split_text=True,
+                            suppress_stdout=True,
+                            pages="all")
+tables_2[0].df.head(25)
+
+# %%
+# PDFLayoutTextStripper
+# https://stackoverflow.com/questions/51334387/unable-to-launch-gateway-from-python-in-py4j
+# https://www.py4j.org/py4j_java_gateway.html#examples
+
+gg = (JavaGateway
+      .launch_gateway(
+          classpath="pdf_layout_text_stripper/python-gateway.jar",
+          die_on_exit=True))
+
+pdf_stripper = gg.jvm.io.github.jonathanlink.PythonGateway()
+
+# %%
+res_1 = pdf_stripper.strip(PDF_1)
+str_1 = res_1["payload"].splitlines()
+for i in range(30):
+    print(str_1[i])
+
+# %%
+res_2 = pdf_stripper.strip(PDF_2)
+str_2 = res_2["payload"].splitlines()
+for i in range(30):
+    print(str_2[i])
